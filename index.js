1a8617b5ffc6301dad7a43e4aa42361198e89b2e
jutivia <rukkylaw.tl@gmail.com>
2021-08-20 02:16:36 +0100

    html added

---
+++ index.js
@@ -0,0 +1,6 @@
+const printName=(firstName, lastName)=>{
+    console.log(`My name is ${firstName} ${lastName}`)
+}
+
+
+document.getElementById('name').textContent=printName(printName('Olajumoke', 'Boladale-Lawal'))
